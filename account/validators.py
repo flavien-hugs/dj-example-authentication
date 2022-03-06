b06c967a30b71c1bfc37ec79278202e6230222f9
flavien-hugs <geekactux@gmail.com>
2022-03-06 18:40:55 +0000

    edit password validator and specify on settings

---
+++ account/validators.py
@@ -0,0 +1,15 @@
+# account.validators.py
+
+from django.core.exceptions import ValidationError
+
+
+class ContainsLetterValidator:
+
+    def validate(self, password, username):
+        if not any(char.isalpha() for char in password):
+            raise ValidationError(
+                'Le mot de passe doit contenir une lettre', code='password_no_letters'
+            )
+
+    def get_help_text(self):
+        return 'Votre mot de passe doit contenir au moins une lettre majuscule ou minuscule.'
--- core/settings.py
+++ core/settings.py
@@ -114,14 +114,12 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        'OPTIONS': {'max_similarity': 0.9}},
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {'min_length': 9}
     },
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME': 'account.validators.ContainsLetterValidator',
+    },
 ]
 
 # Hashage des donnees
