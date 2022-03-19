517c1af9e3403fec685512e09cbdba2f50588b03
flavien-hugs <geekactux@gmail.com>
2022-03-19 07:07:53 +0000

    improve static file

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,30 +1,44 @@
-
 $(document).ready(function() {
-	'use strict';
+    'use strict';
 
-	/*--------------------------------------------
-	togglePassword
-	--------------------------------------------*/
+    /*--------------------------------------------
+    togglePassword
+    --------------------------------------------*/
 
-  function togglePassword(){
-      let input = document.getElementById("inputPass");
-      var eye = document.getElementById("eye");
-      var eyeSlash = document.getElementById("eye-slash");
+    function togglePassword() {
+        let input = document.getElementById("inputPass");
+        var eye = document.getElementById("eye");
+        var eyeSlash = document.getElementById("eye-slash");
 
-      if(input.type === "password"){
-          input.type = "text"
-          eye.style.display = "none";
-          eyeSlash.style.display = "inline";
-      } else {
-          input.type = "password"
-          eye.style.display = "inline";
-          eyeSlash.style.display = "none";
-      }
-  }
+        if (input.type === "password") {
+            input.type = "text"
+            eye.style.display = "none";
+            eyeSlash.style.display = "inline";
+        } else {
+            input.type = "password"
+            eye.style.display = "inline";
+            eyeSlash.style.display = "none";
+        }
+    }
 
-  $('.pass-toggler-btn').on('click', 'i', function() {
-  	togglePassword();
-  })
+    $('.pass-toggler-btn').on('click', 'i', function() {
+        togglePassword();
+    })
 
 
+    /*--------------------------------------------
+    Owl Carousel
+    --------------------------------------------*/
+
+    $('.testimonial-carousel').owlCarousel({
+        loop: true,
+        items: 1,
+        autoplay: true,
+        autoplayHoverPause: true,
+        dots: true,
+        nav: false,
+        autoplaySpeed: 600,
+        dotsSpeed: 600,
+        margin: 15
+    })
 })
