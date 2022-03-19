7e63363062f3c4a5b6bf000b9048e1594ddab61c
flavien-hugs <geekactux@gmail.com>
2022-03-19 19:50:06 +0000

    improve utilitary file

--- helpers/utilitary.py
+++ helpers/utilitary.py
@@ -43,7 +43,11 @@ def get_client_ip(request):
 
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, date_joined):
-        return (six.text_type(user.pk) + six.text_type(date_joined) + six.text_type(user.is_active))
+        return (
+            six.text_type(user.pk)
+            + six.text_type(date_joined)
+            + six.text_type(user.is_active)
+        )
 
 
 generate_token = TokenGenerator()
